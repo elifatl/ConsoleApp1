b588a61c2414c077e94ee8c6adab338e05a585f3
ELİF ATLI <elif_hamartia123@hotmail.com>
2020-10-27 22:16:08 +0300

    iki sayıdan büyük olanı yazdıran

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,8 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //deneme yapıyorum 2223
+            int x, y;
+            Console.Write("1.Sayıyı Gir:");
+            x = Convert.ToInt16(Console.ReadLine());
+            Console.Write("2.Sayıyı Gir:");
+            y = Convert.ToInt16(Console.ReadLine());
+            if (x > y)
+                Console.WriteLine(x);
+            else
+                Console.WriteLine(y);
+            Console.Read();
         }
     }
 }
